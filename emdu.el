465f56474fd19689b6a38d8a5c4533088eed57c0
Elephant Liu <lexdene@gmail.com>
2024-02-28 15:45:10 +0800

    add short issue command

--- emdu.el
+++ emdu.el
@@ -26,3 +26,27 @@
                     repo (match-string 3 url)
                     issue-number (match-string 5 url))
               (format "[%s#%s](%s)" repo issue-number url)))))))
+
+(defun emdu-short-issue (url)
+  "insert issue in short format"
+  (interactive
+    (list
+      (read-from-minibuffer "issue url:")))
+  (progn
+    (let ((text (emdu-issue-url-to-short url)))
+      (if text
+        (insert text)
+        (message "format error")))))
+
+(defun emdu-issue-url-to-short (url)
+  (progn
+    (let
+      ((repo "")
+       (issue-number ""))
+      (save-match-data
+        (and (string-match "https://\\([^/]+\\)/\\([^/]+/[^/]+\\)/\\(pull\\|issues\\)/\\([0-9]+\\)" url)
+          (progn
+            (setq
+              repo (match-string 2 url)
+              issue-number (match-string 4 url))
+            (format "%s#%s" repo issue-number)))))))
